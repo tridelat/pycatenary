67318d9a238c853d7e9066a562c609d5fe6704a9
Tristan de Lataillade <delataillade.tristan@gmail.com>
2020-04-24 11:56:47 -0400

    Tag version 0.3

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.md') as file:
 
 setup(name='pycatenary',
       packages=['pycatenary'],
-      version='0.2',
+      version='0.3',
       description='A Python library for solving catenary equations',
       long_description=long_description,
       long_description_content_type='text/markdown',
       author='Tristan de Lataillade',
       author_email='delataillade.tristan@gmail.com',
       url='https://github.com/tridelat/pycatenary',
-      download_url='https://github.com/tridelat/pycatenary/archive/v0.2.tar.gz'
+      download_url='https://github.com/tridelat/pycatenary/archive/v0.3.tar.gz',
       keywords=['catenary', 'mooring', 'python'],
       classifiers=["Programming Language :: Python :: 3",
                    "License :: OSI Approved :: MIT License",
