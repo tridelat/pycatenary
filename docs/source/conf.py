e7ff27664f58a6a8617c15ce53242819c08f3abd
Tristan de Lataillade <delataillade.tristan@gmail.com>
2021-09-20 22:39:48 +0200

    Release v0.4.0 (switch to semantic versioning)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Tristan de Lataillade'
 author = 'Tristan de Lataillade'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.4.0'
 
 
 # -- General configuration ---------------------------------------------------
